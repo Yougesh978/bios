55a6b9e45898138dee358686da3265636eda1270
Yougesh978 <yougesh978@gmail.com>
2022-11-13 19:35:23 +0530

    Add Selenum Task

--- Python Tasks/selenium.pyc.py
+++ Python Tasks/selenium.pyc.py
@@ -1,8 +1,28 @@
 from selenium import webdriver
-PATH="C:\Program Files (x86)\chromedriver.exe"
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+import time
 
+PATH="C:\Program Files (x86)\chromedriver.exe"
+#filepath ="C:\Users\USER\OneDrive\Desktop\Pentest\shellscript.txt"
 driver = webdriver.Chrome(PATH)
 
-driver.get("https://www.google.com/")
+driver.get("http://the-internet.herokuapp.com/upload")
 print(driver.title)
+
+search = driver.find_element(By.ID, "file-upload")
+#search.clear()
+search.send_keys(r"C:\Users\USER\OneDrive\Desktop\Pentest\logo.png")   #this for the upload 
+#search.send_keys("getting started with python")
+#search.send_keys(Keys.RETURN)
+#search.send_keys(Keys.RETURN)
+# print(driver.page_source) to get the source code as an output
+upload=driver.find_element(By.ID, "file-submit")       #for submit button
+upload.click()
+
+result=driver.find_element(By.TAG_NAME, "h3")
+print(result.text)
+
+time.sleep(15)
+
 driver.quit()
